de2e2d59a1c3b74b9611a90ec5e9f930cd0eeb74
Gabriele Pappalardo <gabriele.pappalardo@jetbrains.com>
2024-04-25 16:12:02 -0400

    Add new autoloaded `SceneManager` component to change scenes while playing

---
+++ src/main/kotlin/me/gabryon/kodeedelivery/annotations/Autoload.kt
@@ -0,0 +1,8 @@
+package me.gabryon.kodeedelivery.annotations
+
+/**
+ * A marker annotation indicating that a class is autoloaded by Godot.
+ */
+@Target(AnnotationTarget.CLASS)
+@MustBeDocumented
+annotation class Autoload
