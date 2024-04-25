8f1e0f15617cbac71da739369a975192c57fab70
Gabriele Pappalardo <gabriele.pappalardo@jetbrains.com>
2024-04-25 16:12:02 -0400

    Add new autoloaded `SceneManager` component to change scenes while playing

---
+++ src/main/kotlin/me/gabryon/kodeedelivery/managers/SceneManager.kt
@@ -0,0 +1,29 @@
+package me.gabryon.kodeedelivery.managers
+
+import godot.Node
+import godot.annotation.RegisterClass
+import godot.annotation.RegisterFunction
+import godot.core.asNodePath
+import me.gabryon.kodeedelivery.annotations.Autoload
+
+enum class Scene(val scenePath: String) {
+    StartGame("res://scenes/start-scene.tscn"),
+    MainGame("res://scenes/main.tscn"),
+    EndGame("res://scenes/end-scene.tscn"),
+    About("res://scenes/about.tscn"); // TODO: to implement!
+}
+
+@Autoload
+@RegisterClass
+class SceneManager : Node() {
+    @RegisterFunction
+    fun changeTo(scene: Scene) {
+        getTree()?.changeSceneToFile(scene.scenePath)
+    }
+}
+
+
+val Node.sceneManager: SceneManager
+    get() {
+        return getNode("/root/SceneManagement".asNodePath()) as SceneManager
+    }
