50c71401b8cbd06f12a199b794e040f1934773a7
Gabriele Pappalardo <gabriele.pappalardo@jetbrains.com>
2024-05-15 16:57:26 +0200

    Update "About" button to open browser with link

--- src/main/kotlin/me/gabryon/kodeedelivery/ui/OpenAboutButton.kt
+++ src/main/kotlin/me/gabryon/kodeedelivery/ui/OpenAboutButton.kt
@@ -1,9 +1,16 @@
 package me.gabryon.kodeedelivery.ui
 
+import godot.Button
+import godot.OS
 import godot.annotation.RegisterClass
+import godot.annotation.RegisterFunction
 import me.gabryon.kodeedelivery.managers.Scene
+import me.gabryon.kodeedelivery.utility.ABOUT_LINK
 
 @RegisterClass
-class OpenAboutButton : ChangeSceneButtonLogic(
-    sceneToLoad = Scene.About,
-)
+class OpenAboutButton : Button() {
+    @RegisterFunction
+    fun openAboutLink() {
+        OS.shellOpen(ABOUT_LINK)
+    }
+}
